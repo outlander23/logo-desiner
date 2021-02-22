ef037d5312d9c3b69b298c509800fc78e0bca16f
Mahmudul Alam <mahmudula2000@gmail.com>
2021-02-22 08:17:11 +0600

    another try

---
+++ src/HomePage/Start/hireNow.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+import "./css.css"
+
+class Service extends Component {
+
+  render() {
+    return (
+        <div id="special-footer">
+
+          <div className="footer-top">
+            <h3>Start Creating Your Logo Today With Me !</h3>
+            <p>Choose vogue Creative Logo for Professionals.</p>
+            <a href="https://bit.ly/3taICLS" class="button">Hire Now</a>
+
+          </div>
+        </div>
+    );
+  }
+}
+
+export default Service;
